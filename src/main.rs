311659369d9247f51401a508aa6085044d15160b
LittleB0xes <nihenriot@gmail.com>
2022-09-09 22:54:45 +0200

    Add render_text function

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use sdl2::event::Event;
 use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode;
 use sdl2::image::InitFlag;
+use sdl2::render::{WindowCanvas, TextureCreator};
 
 use rand::prelude::*;
+use sdl2::ttf::Font;
+use sdl2::video::WindowContext;
 
 use std::time::{Instant, Duration};
 use std::f32;
 
 const WIDTH: f32 = 640.0;
+const UI_WIDTH: u32 = 200;
 const HEIGHT: f32 = 640.0;
+
 
 struct Cell {
     x: f32,
@@ -86,14 +91,47 @@ fn rules(color_a: Color, color_b: Color) -> f32 {
     }
 }
 
+fn render_text(canvas: &mut WindowCanvas, texture_creator: &TextureCreator<WindowContext>, font: &Font, text: &str, color: Color) -> Result<(), String>{
+    let text_size = font.size_of(text).unwrap();
+
+    let surface = font
+        .render(text)
+        .blended(color)
+        .map_err(|e| e.to_string())?;
+
+
+    let texture = texture_creator
+        .create_texture_from_surface(&surface)
+        .map_err(|e| e.to_string())?;
+
+
+    //let TextureQuery { width, height, .. } = texture.query();
+
+    let target = Rect::new(
+    WIDTH as i32 + 10 ,
+    0,
+    text_size.0,
+    text_size.1,
+    );
+
+    canvas.copy(&texture, None, Some(target))?;
+
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
 
     let sdl_context = sdl2::init()?;
+
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+
+    // load font
+    let font = ttf_context.load_font("./assets/Hack-Regular.ttf", 16)?;
+
     let video_subsystem = sdl_context.video()?;
     let _image_context = sdl2::image::init(InitFlag::PNG)?;
 
-    let window = video_subsystem.window("Cell Life", WIDTH as u32, HEIGHT as u32)
+    let window = video_subsystem.window("Cell Life", UI_WIDTH + WIDTH as u32, HEIGHT as u32)
         .position_centered()
         .allow_highdpi()
         .resizable()
@@ -102,7 +140,12 @@ fn main() -> Result<(), String> {
         .expect("Could not initialize video subsystem");
     
     let mut canvas = window.into_canvas().build().expect("Could not create a canvas");
-    canvas.set_logical_size(WIDTH as u32, HEIGHT as u32).expect("Logical Size Error");
+    canvas.set_logical_size(UI_WIDTH + WIDTH as u32, HEIGHT as u32).expect("Logical Size Error");
+
+    let texture_creator = canvas.texture_creator();
+
+
+    
 
     let mut event_pump = sdl_context.event_pump()?;
 
@@ -110,27 +153,27 @@ fn main() -> Result<(), String> {
 
     // Spawn some randomized particles
     let mut particles: Vec<Cell> = Vec::new();
-    for _i in 0..1000 {
+    for _i in 0..1500 {
         let x: u32 = rng.gen::<u32>() % (WIDTH as u32);
         let y: u32 = rng.gen::<u32>() % (HEIGHT as u32);
 
         particles.push(Cell::new(x as f32, y as f32, Color::YELLOW));
     }
 
-    for _i in 0..1000 {
+    for _i in 0..1500 {
         let x: u32 = rng.gen::<u32>() % (WIDTH as u32);
         let y: u32 = rng.gen::<u32>() % (HEIGHT as u32);
 
         particles.push(Cell::new(x as f32, y as f32, Color::BLUE));
     }
     
-    for _i in 0..1000 {
+    for _i in 0..1500 {
         let x: u32 = rng.gen::<u32>() % (WIDTH as u32);
         let y: u32 = rng.gen::<u32>() % (HEIGHT as u32);
 
         particles.push(Cell::new(x as f32, y as f32, Color::RED));
     }
-    for _i in 0..1000 {
+    for _i in 0..1500 {
         let x: u32 = rng.gen::<u32>() % (WIDTH as u32);
         let y: u32 = rng.gen::<u32>() % (HEIGHT as u32);
 
@@ -138,7 +181,7 @@ fn main() -> Result<(), String> {
     }
     // Main Loop
     'running: loop {
-        //let begining = Instant::now();
+        let begining = Instant::now();
 
         // Updating
         interaction(&mut particles);
@@ -146,7 +189,7 @@ fn main() -> Result<(), String> {
         // Rendering
         canvas.clear();
         canvas.set_draw_color(Color::RGB(0,0,0));
-        canvas.fill_rect(Rect::new(0,0,640, 640))?;
+        canvas.fill_rect(Rect::new(0,0,WIDTH as u32 + UI_WIDTH, HEIGHT as u32))?;
 
         for part in particles.iter() {
             let destination_rect = Rect::new(part.x as i32, part.y as i32, 2, 2);
@@ -165,11 +208,13 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        //let end = Instant::now();
-        //println!("{}", 1000 / (end - begining).as_millis());
+        let end = Instant::now();
+        let tps = format!("TPF: {}",  (end - begining).as_millis());
+        
 
+        render_text(&mut canvas, &texture_creator, &font, &tps, Color::WHITE);
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
     Ok(())
 }
